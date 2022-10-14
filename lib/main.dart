489188e1f0d3e7148d20eb196605f66b6b152ae6
RUSHIL <69619062+rushilmakvana@users.noreply.github.com>
2022-10-15 00:38:56 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -61,6 +61,14 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme:ThemeData(
+        useMaterial3:true,
+        pageTransitionTheme: PageTransitionTheme(
+          builders:<TargetPlatform,PageTransitionBuilder>{
+             TargetPlatfrom.android : FadeUpwordPageTransitionBuilder(),
+            },
+          ),
+      ),
       home: Scaffold(
         appBar: AppBar(
           title: Text('First App'),
